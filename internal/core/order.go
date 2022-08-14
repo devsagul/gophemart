29f276e764e84a4dde276ee528d2ed05041d7773
S. Guliaev <dev.sagul@gmail.com>
2022-08-15 01:56:41 +0300

    add accrual for order

--- internal/core/order.go
+++ internal/core/order.go
@@ -26,7 +26,7 @@ type Order struct {
 	Status     OrderStatus     `json:"status"`
 	UploadedAt time.Time       `json:"uploaded_at"`
 	UserId     uuid.UUID       `json:"-"`
-	Accrual    decimal.Decimal `json:"-"`
+	Accrual    decimal.Decimal `json:"accrual"`
 }
 
 func NewOrder(id string, user *User, uploadedAt time.Time) (*Order, error) {
